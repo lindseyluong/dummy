08c3d09c4987f985f7b7f4e3f456e0a62c9873c4
Armando <armando.picazo2@gmail.com>
2024-03-07 19:07:21 -0600

    nav bar

--- App.js
+++ App.js
@@ -5,65 +5,52 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
 
-function ExploreGenre({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-}
+// function ExploreGenre({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+//     </View>
+//   );
+// }
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{flex: 3, backgroundColor: '#ADD8E6' }}>
-      <View style={{flex: 1, backgroundColor: 'AE3D25'}}>
-        <Text>Home!</Text>
-      </View>
-      <View style={{flex: 2, backgroundColor: '#B323C1'}}>
-        <Text>MIDDLE</Text>
-      </View>
-      <View style={{ justifyContent: 'space-around', flexDirection:'row', backgroundColor: '#FFFFFF'}}>
-        <View style={{ backgroundColor: '#E50B0B', width: '30%'}}>
-          <Button title="Heart" 
-          onPress={() => navigation.navigate('Messages')}/>
-        </View>
-        <View style={{ backgroundColor: '#E84ACE', width: '30%'}}>
-          <Button title="Heart" 
-          onPress={() => navigation.navigate('Messages')}/>
-        </View>
-      </View>
-    </View>
-  );
-}
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//       <Button title="Go to Home" 
+//       onPress={() => navigation.navigate('Home')} />
+//     </View>
+//   );
+// }
 
-function MessageScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-      <Button title="Go to Home" 
-      onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-}
+// function MessageScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//       <Button title="Go to Home" 
+//       onPress={() => navigation.navigate('Home')} />
+//     </View>
+//   );
+// }
 
-function Preferences({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-}
+// function Preferences({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+//     </View>
+//   );
+// }
 
-function Events({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-}
+// function Events({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+//     </View>
+//   );
+// }
 
 
 
@@ -73,9 +60,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Messages" component={MessageScreen} options={{
-          tabBarLabel: 'Homie'
-        }} />
+        <Tab.Screen name="Messages" component={MessageScreen} />
         <Tab.Screen name="Preferences" component={Preferences} />
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Events" component={Events} />
